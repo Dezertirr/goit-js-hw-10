df8831ac2a63bcf0ee1a31d5a6d3a8bda6f0f248
Dezertirr <125862316+Dezertirr@users.noreply.github.com>
2023-05-08 23:14:11 +0300

    push

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import './css/styles.css';
-import Notiflix from "notiflix";
+import { debounce } from 'lodash';
+import {fetchCountries} from './js/fetchCountries.js'
+
+export { countryInput };
 
 const DEBOUNCE_DELAY = 300;
-const MAX_RESULTS = 10;
+export const MAX_RESULTS = 10;
 
 const countryInput = document.getElementById('search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
-function createCountryList(country) {
+export function createCountryList(country) {
     const listItem = document.createElement('li');
     listItem.innerHTML = `<img class='country-flag' src ='${country.flags.svg}' alt='Flag of ${country.name.official}' width='30' /><p>${country.name.official}</p>`;
     listItem.addEventListener('click', () => {
@@ -18,7 +21,7 @@ function createCountryList(country) {
     return listItem;
   }
 
-function showCountry(country) {
+export function showCountry(country) {
     const countryInfoHTML = `
         <h2>${country.name.official}</h2>
         <p><span>Capital:</span> ${country.capital}</p>
@@ -29,55 +32,7 @@ function showCountry(country) {
     countryInfo.innerHTML = countryInfoHTML;
 }
 
-function searchInput() {
-  const searchcontry = countryInput.value;
-  if (searchcontry === "") {
-    countryList.innerHTML = "";
-    countryInfo.innerHTML = "";
-    return;
-  }
-  fetch(`https://restcountries.com/v3.1/name/${searchcontry}`)
-    .then((response) => {
-      if (!response.ok) {
-        Notiflix.Notify.failure(`Such a country does not exist:${searchcontry}`);
-throw new Error(`Such a country does not exist:${searchcontry}`);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      countryList.innerHTML = "";
-      if (data.length > MAX_RESULTS) {
-        Notiflix.Notify.warning('Too many matches found. Please enter a more specific name.');
-      } else {
-        data.forEach((country) => {
-          const listItem = createCountryList(country);
-          countryList.appendChild(listItem);
-        });
-        if (data.length === 1) {
-          showCountry(data[0]);
-          while (countryList.firstChild) {
-            countryList.removeChild(countryList.firstChild);
-          }
-        } else {
-          countryInfo.innerHTML = "";
-        }
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-      countryList.innerHTML = "";
-      countryInfo.innerHTML = `<p class="error-message">${error.message}</p>`;
-    });
-}
+countryInput.addEventListener("input", debounce(() => {
+  fetchCountries(countryInput, countryList, countryInfo);
+}, DEBOUNCE_DELAY));
 
-countryInput.addEventListener("input", debounce(searchInput, DEBOUNCE_DELAY));
-
-function debounce(fn, debounceTime) {
-  let timeout;
-  return function (...args) {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => {
-      fn.apply(this, args);
-    }, debounceTime);
-  };
-}
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,47 @@
+import Notiflix from "notiflix";
+import { MAX_RESULTS, createCountryList, showCountry } from '../index.js';
+
+export function fetchCountries(countryInput, countryList, countryInfo) {
+  const searchcontry = countryInput.value.trim();
+  if (searchcontry === "") {
+    countryList.innerHTML = "";
+    countryInfo.innerHTML = "";
+    return;
+  }
+  fetch(`https://restcountries.com/v3.1/name/${searchcontry}`)
+    .then((response) => {
+      if (!response.ok) {
+          Notiflix.Notify.failure(`Such a country does not exist:${searchcontry}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+        countryList.innerHTML = "";
+        if (!Array.isArray(data)) {
+          const country = data;
+          const listItem = createCountryList(country);
+          countryList.appendChild(listItem);
+          showCountry(country, countryInfo);
+        } else if (data.length > MAX_RESULTS) {
+          Notiflix.Notify.warning('Too many matches found. Please enter a more specific name.');
+        } else {
+          data.forEach((country) => {
+            const listItem = createCountryList(country);
+            countryList.appendChild(listItem);
+          });
+          if (data.length === 1) {
+            showCountry(data[0], countryInfo);
+            while (countryList.firstChild) {
+              countryList.removeChild(countryList.firstChild);
+            }
+          } else {
+            countryInfo.innerHTML = "";
+          }
+        }
+      })
+    .catch((error) => {
+      console.log(error);
+      countryList.innerHTML = "";
+      countryInfo.innerHTML = `<p class="error-message">${error.message}</p>`;
+    });
+}
